348f5adabe5ff6536684329d657494ce631514c7
soga-johnny <soga.johnny@gmail.com>
2025-04-07 20:49:43 +0900

    analy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import LayoutProvider from "@/components/LayoutProvider";
 // import Header from '@/components/Header';
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -59,6 +60,7 @@ export default function RootLayout({
       >
         {/* <Header /> */}
         <LayoutProvider>{children}</LayoutProvider>
+        <Analytics />
       </body>
     </html>
   );
